68d992bd9a86f4e55fa07ace8a37088713f7de66
Jannis Morgenstern <jannis.morgenstern@titanom.com>
2022-07-10 12:00:39 +0200

    feat(fish): add stuff to path

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,9 @@ end
 
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
 
-alias nv="nvim"
+alias nv="neovide"
 alias btctl="bluetoothctl"
 alias gs="git status"
 alias gP="git push"
@@ -21,7 +22,11 @@ alias cls="clear"
 alias lsl="ls -l"
 alias lsa="ls -la"
 alias icat="kitty +kitten icat"
+alias fucking="sudo"
 
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+set -gx PNPM_HOME "/home/jannis/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
